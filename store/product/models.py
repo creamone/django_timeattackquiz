2dd26c06e574e0ae0193d7a039029f9fc70212dd
creamone <gsukim777@gmail.com>
2022-06-10 17:16:21 +0900

    06-10

--- store/product/models.py
+++ store/product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    description = models.CharField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class OrderStatus(models.Model):
+    status = models.CharField(max_length=30)
+
+class ProductOrder(models.Model):
